f291161a94511f0f4d5d1f925a4e4d5156660e6f
David <Rigby335@hotmail.com>
2023-11-10 12:24:31 -0800

    feat: added config files for jest/ts - POST test for user fails RED

---
+++ __tests__/users.test.ts
@@ -0,0 +1,12 @@
+import request from "supertest";
+import app from "../src/app";
+
+describe("backend author routes", () => {
+  //   const agent = request.agent(app);
+  it("#POST creates a new user", async () => {
+    const res = await request(app)
+      .post("/users")
+      .send({ email: "test@test.com", password: 123456 });
+    expect(res.status).toBe(200);
+  });
+});
