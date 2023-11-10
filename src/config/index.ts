77dcc3d8a0a5a968305f4916d704011b5f2e688a
David <Rigby335@hotmail.com>
2023-11-10 14:27:58 -0800

    feat: setup UserService and signUp function. controller next (included password:true in obj select - maybe bad?)

---
+++ src/config/index.ts
@@ -0,0 +1,5 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+// export const { SALT_ROUNDS } = process.env;
+export const { JWT_SECRET } = process.env;
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,4 +1,8 @@
+import { hash } from "bcrypt";
 import { prisma } from "../utils/db.server";
+import { sign } from "jsonwebtoken";
+import { JWT_SECRET } from "../config";
+import { Users } from "@prisma/client";
 
 type User = {
   email: string;
@@ -6,7 +10,18 @@ type User = {
 };
 
 export default class UserService {
-  static async signUpUser({ email, password }: User): Promise<User> {
-    // const passwordHash = await
+  static async signUpUser({ email, password }: User): Promise<[Users, string]> {
+    const passwordHash = await hash(password, Number(process.env.SALT_ROUNDS));
+    const user = await prisma.users.create({
+      data: { email, password: passwordHash },
+      select: { id: true, email: true, password: true },
+    });
+
+    const tokenPayLoad = { id: user.id, email: user.email };
+
+    const token = sign(tokenPayLoad, JWT_SECRET || "defaultSecret", {
+      expiresIn: "1 day",
+    });
+    return [user, token];
   }
 }
