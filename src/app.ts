2ece29dd3b734d7259c0f6ee81b2397fa62e03ab
David <Rigby335@hotmail.com>
2023-11-10 11:53:49 -0800

    feat: setup app.ts and server

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+/* eslint-disable no-console */
+import express, { Express } from "express";
+import cors from "cors";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+if (!process.env.PORT) {
+  console.log("No PORT detected!");
+  process.exit(1);
+}
+
+const PORT: number = parseInt(process.env.PORT as string, 10);
+
+const app: Express = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
+export default app;
