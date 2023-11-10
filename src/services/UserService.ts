78d8af9edacf114fdd91c4a0dbe315657da38989
David <Rigby335@hotmail.com>
2023-11-10 14:57:36 -0800

    feat: http-errors installed, error handling for signUp w/ an existing user returns 409

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -3,6 +3,7 @@ import { prisma } from "../utils/db.server";
 import { sign } from "jsonwebtoken";
 import { JWT_SECRET } from "../config";
 import { Users } from "@prisma/client";
+import createHttpError from "http-errors";
 
 type User = {
   email: string;
@@ -11,6 +12,10 @@ type User = {
 
 export default class UserService {
   static async signUpUser({ email, password }: User): Promise<[Users, string]> {
+    const existingUser = await prisma.users.findUnique({
+      where: { email },
+    });
+    if (existingUser) throw createHttpError(409, "Email already exists");
     const passwordHash = await hash(password, Number(process.env.SALT_ROUNDS));
     const user = await prisma.users.create({
       data: { email, password: passwordHash },
