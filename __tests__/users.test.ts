76250548725457b560b09d603eb420a3ab0315c8
David <Rigby335@hotmail.com>
2023-11-14 09:51:24 -0800

    feat: test for DELETE user session fails RED

--- __tests__/users.test.ts
+++ __tests__/users.test.ts
@@ -1,16 +1,16 @@
 import request from 'supertest';
 import app from '../src/app';
-// import { createUser } from '../src/types/users.create';
+import { createUser } from '../src/types/users.create';
 
 const mockUser = { email: 'testing@test.com', password: 'test' };
 
-// async function registerAndLogin(
-//   mockUser: createUser,
-//   agent: request.SuperTest<request.Test>,
-// ) {
-//   const res = await agent.post('/users').send(mockUser);
-//   return res.body;
-// }
+async function registerAndLogin(
+  mockUser: createUser,
+  agent: request.SuperTest<request.Test>,
+) {
+  const res = await agent.post('/users').send(mockUser);
+  return res.body;
+}
 
 describe('backend author routes', () => {
   const agent = request.agent(app);
@@ -24,10 +24,16 @@ describe('backend author routes', () => {
     const res = await agent.get('/users/18');
     expect(res.status).toBe(200);
   });
-  it.only('logs in a user', async () => {
-    // await request(app).post('/users').send(mockUser);
+  it('#POST/users/sessions logs in a user', async () => {
+    await request(app).post('/users').send(mockUser);
     const res = await agent.post('/users/sessions').send(mockUser);
-    console.log('res.body', res);
     expect(res.status).toBe(200);
+  });
+  it.only('#DELETE/users/sessions deletes a user session (logs user out)', async () => {
+    const user = await registerAndLogin(mockUser, agent);
+    const logout = await agent.delete('/users/sessions');
+    expect(logout.status).toBe(200);
+    const res = await agent.get(`/users/${user.id}`);
+    expect(res.status).toBe(401);
   });
 });
